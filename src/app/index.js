127b323c73473f87e8ec8bc9826d639eba2c7173
Alexander Gudulin <alexandr.gudulin@gmail.com>
2016-11-03 11:20:46 +0100

    style the table

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import * as actions from 'common/general/actions'
+import { Board } from 'components'
 
 import './index.css'
 
@@ -9,7 +10,7 @@ class App extends Component {
   render () {
     return (
       <div className='app'>
-        Sudoku
+        <Board />
       </div>
     )
   }
